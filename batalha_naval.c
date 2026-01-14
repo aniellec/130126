05f88e751c90890c6b041b039d788f0f18b35c38
aniellec <ani.casagrande@gmail.com>
2026-01-14 09:31:24 -0300

    Update batalha_naval.c
    versão com 4 navios

--- batalha_naval.c
+++ batalha_naval.c
@@ -32,8 +32,14 @@ int main(){
     int ponta_navio1_coluna;
     int ponta_navio2_linha;
     int ponta_navio2_coluna;
-    int tamanho_navio1;
-    int tamanho_navio2;
+    int ponta_navio3_linha;
+    int ponta_navio3_coluna;
+    int ponta_navio4_linha;
+    int ponta_navio4_coluna;
+    //int tamanho_navio1;
+    int tamanho = 3;
+
+    printf("\n*****Navios 1 e 2: horizontais. Navios 3 e 4: diagonais.*****\n\n");
 
     printf("\nDigite a posicao da ponta do primeiro navio(linha): ");
     scanf("%d", &ponta_navio1_linha);
@@ -41,8 +47,6 @@ int main(){
     printf("\nDigite a posicao da ponta do primeiro navio(coluna): ");
     scanf("%d", &ponta_navio1_coluna);
 
-    printf("\nDigite o tamanho do primeiro navio: ");
-    scanf("%d", &tamanho_navio1);
 
     if (ponta_navio1_linha < 0 || ponta_navio1_linha > 9 ||
         ponta_navio1_coluna < 0 || ponta_navio1_coluna > 9) {
@@ -51,16 +55,16 @@ int main(){
         return 0;
     }
 
-    if (ponta_navio1_coluna + tamanho_navio1 - 1 > 9) {
+    if (ponta_navio1_coluna + tamanho - 1 > 9) {
         printf("O navio cai fora do tabuleiro!\n");
         return 0;
     }
 
-    for (j = ponta_navio1_coluna; j < ponta_navio1_coluna + tamanho_navio1; j++) {
+    for (j = ponta_navio1_coluna; j < ponta_navio1_coluna + tamanho; j++) {
         tabuleiro[ponta_navio1_linha][j] = 3;
     }
 
-    printf("Tabluleiro com primeiro navio: \n");
+    printf("Tabluleiro 1 navio: \n");
 
     for (i = 0; i < 10; i++) {
         for (j = 0; j < 10; j++) {
@@ -75,9 +79,6 @@ int main(){
     printf("\nDigite a posicao da ponta do segundo navio(coluna): ");
     scanf("%d", &ponta_navio2_coluna);
 
-    printf("\nDigite o tamanho do segundo navio: ");
-    scanf("%d", &tamanho_navio2);
-
     if (ponta_navio2_linha < 0 || ponta_navio2_linha > 9 ||
         ponta_navio2_coluna < 0 || ponta_navio2_coluna > 9) {
 
@@ -85,24 +86,23 @@ int main(){
         return 0;
     }
 
-    if (ponta_navio2_coluna + tamanho_navio2 - 1 > 9) {
+    if (ponta_navio2_coluna + tamanho - 1 > 9) {
         printf("O navio cai fora do tabuleiro!\n");
         return 0;
     }
 
-    for (j = ponta_navio2_coluna; j < ponta_navio2_coluna + tamanho_navio2; j++) {
+    for (j = ponta_navio2_coluna; j < ponta_navio2_coluna + tamanho; j++) {
     if (tabuleiro[ponta_navio2_linha][j] != 0) {
         printf("O segundo navio sobrepoe o primeiro navio!\n");
         return 0;
     }
     }
 
-    for (j = ponta_navio2_coluna; j < ponta_navio2_coluna + tamanho_navio2; j++) {
+    for (j = ponta_navio2_coluna; j < ponta_navio2_coluna + tamanho; j++) {
         tabuleiro[ponta_navio2_linha][j] = 3;
     }
 
-
-    printf("Tabluleiro com dois navios: \n");
+    printf("Tabluleiro 2 navios: \n");
 
     for (i = 0; i < 10; i++) {
         for (j = 0; j < 10; j++) {
@@ -110,6 +110,92 @@ int main(){
         }
         printf("\n");
     }
+
+    printf("\nDigite a posicao da ponta do terceiro navio(linha): ");
+    scanf("%d", &ponta_navio3_linha);
+
+    printf("\nDigite a posicao da ponta do terceiro navio(coluna): ");
+    scanf("%d", &ponta_navio3_coluna);
+
+
+    if (ponta_navio3_linha < 0 || ponta_navio3_linha > 9 ||
+        ponta_navio3_coluna < 0 || ponta_navio3_coluna > 9) {
+
+        printf("Posicao inicial invalida!\n");
+        return 0;
+    }
+
+    if (ponta_navio3_linha + tamanho - 1 > 9 ||
+        ponta_navio3_coluna + tamanho - 1 > 9) {
+        printf("O navio diagonal cai fora do tabuleiro!\n");
+        return 0;
+    }
+
+    for (i = 0; i < tamanho; i++) {
+        if (tabuleiro[ponta_navio3_linha + i][ponta_navio3_coluna + i] != 0) {
+            printf("Navio diagonal sobrepoe outro navio!\n");
+            return 0;
+        }
+    }
+
+    for (i = 0; i < tamanho; i++) {
+        tabuleiro[ponta_navio3_linha + i][ponta_navio3_coluna + i] = 3;
+    }
+
+    printf("Tabluleiro 3 navios: \n");
+
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    printf("\nDigite a posicao da ponta do quarto navio(linha): ");
+    scanf("%d", &ponta_navio4_linha);
+
+    printf("\nDigite a posicao da ponta do quarto navio(coluna): ");
+    scanf("%d", &ponta_navio4_coluna);
+
+
+    if (ponta_navio4_linha < 0 || ponta_navio4_linha > 9 ||
+        ponta_navio4_coluna < 0 || ponta_navio4_coluna > 9) {
+
+        printf("Posicao inicial invalida!\n");
+        return 0;
+    }
+
+    if (ponta_navio4_linha + tamanho - 1 > 9 ||
+        ponta_navio4_coluna + tamanho - 1 > 9) {
+        printf("O navio diagonal cai fora do tabuleiro!\n");
+        return 0;
+    }
+
+    for (i = 0; i < tamanho; i++) {
+        if (tabuleiro[ponta_navio4_linha + i][ponta_navio4_coluna + i] != 0) {
+            printf("Navio diagonal sobrepoe outro navio!\n");
+            return 0;
+        }
+    }
+
+    for (i = 0; i < tamanho; i++) {
+        tabuleiro[ponta_navio4_linha + i][ponta_navio4_coluna + i] = 3;
+    }
+
+
+
+
+    printf("Tabluleiro 4 navios: \n");
+
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nFIM\n");
 
 
     return 0;
